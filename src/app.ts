100b136201707b1cb0811760569616b1a333cf33
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-09-06 01:18:05 -0600

    Configure and validate environment variables

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+
+import { envs } from "./config/plugins/envs.plugin";
 import { Server } from "./presentation/server";
 
 ( async () => {
@@ -5,6 +7,10 @@ import { Server } from "./presentation/server";
 })();
 
 function main() {
-    Server.start();
+    // Server.start();
+    console.log(envs.PORT);
+    console.log(envs.MAILER_EMAIL);
+    console.log(envs.MAILER_SECRET_KEY);  
+    console.log(envs.PROD);
 }
 
